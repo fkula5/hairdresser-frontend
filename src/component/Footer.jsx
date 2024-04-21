dc621e2dfd5d814dd3a7af9ae7158828cfc1d65c
KatMat03 <katarzynamaternia@gmail.com>
2024-04-21 21:50:22 +0200

    feat: create footer

---
+++ src/component/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles.css'
+
+function Footer() {
+  return (
+    <div className="footer">
+        <div className='name'>
+            <h1>Cut&Book</h1>
+            <p>e-mail: Cut&Book@gmail.com</p>
+            <p>&copy; 2024 MyWebsite. All rights reserved.</p>
+        </div>
+        <div className='social'>
+            <h2>Information: </h2>
+            <p></p>
+            <a href='https://www.youtube.com/'>Link to our YT</a>
+            <p></p>
+            <a href='https://www.instagram.com/'>Link to our IG</a>
+            <p></p>
+            <a href='https://www.facebook.com/'>Link to our FB</a>
+        </div>
+      
+    </div>
+  );
+}
+
+export default Footer;
